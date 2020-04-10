e2ffc05f702694d656929393f1fde90a46554f57
Danny <luuthanhdannydavidnguyen@gmail.com>
2020-04-09 21:26:03 -0400

    add item stack capability to infusion table

--- src/main/java/danny8208/hungerrings/blocks/InfusionTableTile.java
+++ src/main/java/danny8208/hungerrings/blocks/InfusionTableTile.java
@@ -1,9 +1,67 @@
 package danny8208.hungerrings.blocks;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
-public class InfusionTableTile extends TileEntity {
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class InfusionTableTile extends TileEntity implements ITickableTileEntity {
+    protected LazyOptional<IItemHandler> handler = LazyOptional.of(this::createHandler);
+
     public InfusionTableTile() {
         super(ModBlocks.INFUSION_TABLE_TILE);
+    }
+
+    @Override
+    public void tick() {
+    }
+
+    @Override
+    public void read(CompoundNBT tag) {
+        CompoundNBT invTag = tag.getCompound("inv");
+        handler.ifPresent(h -> ((INBTSerializable<CompoundNBT>)h).deserializeNBT(invTag));
+        super.read(tag);
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT tag) {
+        handler.ifPresent(h -> {
+            CompoundNBT compoundNBT = ((INBTSerializable<CompoundNBT>)h).serializeNBT();
+            tag.put("inv", compoundNBT);
+        });
+        return super.write(tag);
+    }
+
+    private ItemStackHandler createHandler() {
+        return new ItemStackHandler(1) {
+            @Override
+            protected int getStackLimit(int slot, @Nonnull ItemStack stack) {
+                return 1;
+            }
+
+            @Override
+            protected void onContentsChanged(int slot) {
+                markDirty();
+            }
+        };
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (!this.removed && cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return handler.cast();
+        }
+        return super.getCapability(cap, side);
     }
 }
