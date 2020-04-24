146c1c4e82234229902aec6c68962808184022a2
Danny <luuthanhdannydavidnguyen@gmail.com>
2020-04-24 19:04:51 -0400

    add hunger processor block drops and json files

--- src/main/java/danny8208/hungerrings/blocks/processor/HungerProcessor.java
+++ src/main/java/danny8208/hungerrings/blocks/processor/HungerProcessor.java
@@ -4,8 +4,16 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 
 import javax.annotation.Nullable;
@@ -29,5 +37,25 @@ public class HungerProcessor extends Block {
     @Override
     public TileEntity createTileEntity(BlockState state, IBlockReader world) {
         return new HungerProcessorTile();
+    }
+
+    @Override
+    public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult p_225533_6_) {
+        if (!worldIn.isRemote) {
+            HungerProcessorTile tile = (HungerProcessorTile)worldIn.getTileEntity(pos);
+            if (player.getActiveItemStack().getItem() == Items.MILK_BUCKET) {
+            }
+        }
+        return super.onBlockActivated(state, worldIn, pos, player, handIn, p_225533_6_);
+    }
+
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
+        super.onBlockHarvested(worldIn, pos, state, player);
+        if (!worldIn.isRemote && worldIn.getTileEntity(pos) instanceof HungerProcessorTile) {
+            HungerProcessorTile tile = (HungerProcessorTile) worldIn.getTileEntity(pos);
+            worldIn.addEntity(new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), tile.inventory.getStackInSlot(0)));
+            worldIn.addEntity(new ItemEntity(worldIn, pos.getX(), pos.getY(), pos.getZ(), tile.inventory.getStackInSlot(1)));
+        }
     }
 }
